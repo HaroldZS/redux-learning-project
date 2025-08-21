889f66658e7e84dc34ad0630cc0ecf9173d0d8e8
Harold Zurita Simon <101837004+HaroldZS@users.noreply.github.com>
2025-08-21 18:21:01 +0200

    feat: Implement PokeAPI fetcher

--- src/components/PokemonList.jsx
+++ src/components/PokemonList.jsx
@@ -2,10 +2,12 @@ import PokemonCard from "./PokemonCard";
 import "./PokemonList.css";
 
 const PokemonList = ({ pokemons = Array(10).fill("") }) => {
+  console.log(pokemons);
+
   return (
     <div className="PokemonList">
       {pokemons.map((pokemon, index) => {
-        return <PokemonCard key={index} />;
+        return <PokemonCard name={pokemon.name} key={index} />;
       })}
     </div>
   );
