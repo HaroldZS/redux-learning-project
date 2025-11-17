f600697e0d88d5a283b87d6b3295c0f013dd4479
Harold Zurita Simon <101837004+HaroldZS@users.noreply.github.com>
2025-11-17 00:48:10 -0400

    feat: Implement mark as favorite

--- src/components/PokemonList.jsx
+++ src/components/PokemonList.jsx
@@ -5,7 +5,7 @@ const PokemonList = ({ pokemons }) => {
   return (
     <div className="PokemonList">
       {pokemons.map((pokemon, index) => {
-        return <PokemonCard name={pokemon.name} image={pokemon.sprites.front_default} key={index} />;
+        return <PokemonCard name={pokemon.name} image={pokemon.sprites.front_default} key={index} types={pokemon.types} id={pokemon.id} favorite={pokemon.favorite} />;
       })}
     </div>
   );
